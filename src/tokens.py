272f738015d7b655f18582771cc55e30e1ee02ab
Pedraza Vázquez, Francisco <fpedrazav02@gmail.com>
2025-07-20 23:28:16 +0200

    feat(Tokens): Implement simple token class for the scanner

---
+++ src/tokens.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import override
+
+
+class TokenType(Enum):
+    LEFT_PAREN = auto()
+    RIGHT_PAREN = auto()
+    LEFT_BRACE = auto()
+    RIGHT_BRACE = auto()
+    COMMA = auto()
+    DOT = auto()
+    MINUS = auto()
+    PLUS = auto()
+    SEMICOLON = auto()
+    SLASH = auto()
+    STAR = auto()
+    BANG = auto()
+    BANG_EQUAL = auto()
+    EQUAL = auto()
+    EQUAL_EQUAL = auto()
+    GREATER = auto()
+    GREATER_EQUAL = auto()
+    LESS = auto()
+    LESS_EQUAL = auto()
+    IDENTIFIER = auto()
+    STRING = auto()
+    NUMBER = auto()
+    AND = auto()
+    CLASS = auto()
+    ELSE = auto()
+    FALSE = auto()
+    FUN = auto()
+    FOR = auto()
+    IF = auto()
+    NIL = auto()
+    OR = auto()
+    PRINT = auto()
+    RETURN = auto()
+    SUPER = auto()
+    THIS = auto()
+    TRUE = auto()
+    VAR = auto()
+    WHILE = auto()
+    EOF = auto()
+
+
+@dataclass
+class Token:
+    _line: int
+    _tokenType: TokenType
+    _literal: object
+    _lexeme: str
+
+    @override
+    def __str__(self) -> str:
+        return "%s %s %s" % (self._tokenType, self._lexeme, self._literal) 
